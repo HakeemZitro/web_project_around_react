5987111db2131c61adb43ea0a41616fda5ddce86
Hakeem Ortiz <hzitrofelix@gmail.com>
2025-12-11 18:46:07 -0600

    feat: Like/Unlike Card and Delete Card working properly with API

--- src/Main/components/Card/Card.jsx
+++ src/Main/components/Card/Card.jsx
@@ -8,7 +8,7 @@ export default function Card(props) {
   return (
     <li className="element">
       <img src={card.link} alt={`Fotografía de ${card.name}`} className="element__image" onClick={() => openFull({ children: <ImagePopup onClose={onClose} card={card} /> })} />
-      <img src={trash} alt="Icono para eliminar post" className="element__trash" onClick={() => openFull({ children: <RemoveCard title="¿Estás seguro?" onClose={onClose} onCardDelete={onCardDelete} /> })} />
+      <img src={trash} alt="Icono para eliminar post" className="element__trash" onClick={() => openFull({ children: <RemoveCard title="¿Estás seguro?" onClose={onClose} onCardDelete={onCardDelete} card={card} /> })} />
       <div className="element__info">
         <p className="element__name">{card.name}</p>
         <button type="button" className={!card.isLiked ? "element__like" : "element__like element__like_active"} onClick={() => onCardLike(card)}></button>
--- src/Main/components/RemoveCard/RemoveCard.jsx
+++ src/Main/components/RemoveCard/RemoveCard.jsx
@@ -2,13 +2,13 @@ import closeIcon from '../../../assets/Close_Icon.svg';
 import Popup from '../Popup/Popup';
 
 export default function RemoveCard(props) {
-  const { onClose, title, onCardDelete } = props;
+  const { onClose, title, card, onCardDelete } = props;
 
   return (
     <Popup>
       <form className="popup__form">
         <h2 className="popup__title">{title}</h2>
-        <button type="submit" className="popup__button" onClick={onCardDelete}>Si</button>
+        <button type="submit" className="popup__button" onClick={() => onCardDelete(card)}>Si</button>
         <img src={closeIcon} alt="Icono para cerrar la ventana emergente" className="popup__close-button" onClick={onClose} />
       </form>
     </Popup>
